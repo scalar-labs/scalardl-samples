a8caa0b6a02ce222fb5f99bdd0efb7787e9a067f
Craig Pastro <siyopao@gmail.com>
2019-02-01 11:29:54 +0900

    Create a script to start scalar DL

---
+++ start.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+print_usage_and_exit() {
+  echo "Usage: $0 [-hs]"
+  echo "  -s apply schema"
+  echo "  -h display help"
+  exit 1
+}
+
+while getopts 'hs' opt; do
+  case $opt in 
+    s) apply_schema=true ;;
+    *) print_usage_and_exit ;;
+  esac
+done
+
+docker-compose up -d
+
+if [ $apply_schema ]; then
+  echo "applying schema"
+  for i in {1..7}; do
+    sleep 3
+    docker-compose exec cassandra cqlsh -f /create_schema.cql && break
+    echo "error applying schema... sleeping for 3 seconds"
+
+    if [ $i -eq 7 ]; then
+      echo "error applying schema... aborting"
+      docker-compose down
+      exit 1
+    fi
+  done
+fi
+
