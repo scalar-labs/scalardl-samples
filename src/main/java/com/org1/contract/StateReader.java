3b796ee34f52bc2c88e9d68944ec5be8d91469a0
Plenty Su <plenty.su@gmail.com>
2022-08-03 23:56:51 +0900

    Upgrade Scalar DL version to 3.5.0 (#57)

--- src/main/java/com/org1/contract/StateReader.java
+++ src/main/java/com/org1/contract/StateReader.java
@@ -1,48 +1,41 @@
 package com.org1.contract;
 
-import com.scalar.dl.ledger.asset.Asset;
-import com.scalar.dl.ledger.asset.InternalAsset;
-import com.scalar.dl.ledger.contract.Contract;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.scalar.dl.ledger.contract.JacksonBasedContract;
 import com.scalar.dl.ledger.exception.ContractContextException;
-import com.scalar.dl.ledger.database.Ledger;
-import java.util.Base64;
+import com.scalar.dl.ledger.statemachine.Asset;
+import com.scalar.dl.ledger.statemachine.Ledger;
 import java.util.Optional;
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
+import javax.annotation.Nullable;
 
-public class StateReader extends Contract {
+/**
+ * It is recommended to use JacksonBasedContract for taking a good balance between development
+ * productivity and performance.
+ */
+public class StateReader extends JacksonBasedContract {
 
+  @Nullable
   @Override
-  public JsonObject invoke(Ledger ledger, JsonObject argument, Optional<JsonObject> properties) {
-    if (!argument.containsKey("asset_id")) {
+  public JsonNode invoke(
+      Ledger<JsonNode> ledger, JsonNode argument, @Nullable JsonNode properties) {
+    if (!argument.has("asset_id")) {
       // ContractContextException is the only throwable exception in a contract and
       // it should be thrown when a contract faces some non-recoverable error
-      throw new ContractContextException("please set asset_id in the argument");
+      throw new ContractContextException("please set asset_id and state in the argument");
     }
 
-    String assetId = argument.getString("asset_id");
-    Optional<Asset> asset = ledger.get(assetId);
-    if (!asset.isPresent()) {
-      return null;
-    }
-    InternalAsset internal = (InternalAsset) asset.get();
+    String assetId = argument.get("asset_id").asText();
+    Optional<Asset<JsonNode>> asset = ledger.get(assetId);
 
-    Base64.Encoder encoder = Base64.getEncoder();
-    JsonObjectBuilder builder =
-        Json.createObjectBuilder()
-            .add("age", internal.age())
-            .add("input", internal.input())
-            .add("output", internal.data())
-            .add("contract_id", internal.contractId())
-            .add("argument", internal.argument())
-            .add("signature", encoder.encodeToString(internal.signature()))
-            .add("hash", encoder.encodeToString(internal.hash()));
-    if (internal.prevHash() != null) {
-      builder.add("prev_hash", encoder.encodeToString(internal.prevHash()));
-    } else {
-      builder.add("prev_hash", "");
-    }
-    return builder.build();
+    return asset
+        .map(
+            value ->
+                (JsonNode)
+                    getObjectMapper()
+                        .createObjectNode()
+                        .put("id", value.id())
+                        .put("age", value.age())
+                        .set("output", value.data()))
+        .orElse(null);
   }
 }
